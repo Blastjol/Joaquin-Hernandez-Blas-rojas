4cca1aa3dfbefd8ac1c1205570939de9ef60157e
Blastjol <163176801+Blastjol@users.noreply.github.com>
2026-09-06 22:45:38 -0300

    Add comments and imports to punto_flotante.py
    Added header comments and import statements.

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -1,1 +1,6 @@
+#JOAQUIN HERNANDEZ Y BLAS ROJAS
+#respuesta a preguntas B1, B2 y B4
+import numpy as np
+from cargar_datos import cargar_serie
+from errores import redondear_cifras_significativas, error_absoluto
 
